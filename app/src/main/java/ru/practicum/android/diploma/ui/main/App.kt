3915417e72ff88123ed11063234c77fe4afade0a
Dmitry-Schukin <iliakirsanov5321@gmail.com>
2026-01-25 20:22:43 +0300

    Исправление предупреждения ["detekt"]: ImportOrdering3

--- app/src/main/java/ru/practicum/android/diploma/ui/main/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/main/App.kt
@@ -1,11 +1,11 @@
 package ru.practicum.android.diploma.ui.main
 
 import android.app.Application
-import org.koin.core.context.GlobalContext.startKoin
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
 import ru.practicum.android.diploma.di.dataModule
-import ru.practicum.android.diploma.di.repositoryModule
 import ru.practicum.android.diploma.di.interactorModule
+import ru.practicum.android.diploma.di.repositoryModule
 import ru.practicum.android.diploma.di.viewModelModule
 
 class App : Application() {
