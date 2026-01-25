a66b2bbb2374fffe16d336d33394bf14cbcc4849
Dmitry-Schukin <iliakirsanov5321@gmail.com>
2026-01-25 19:15:47 +0300

    Исправление предупреждений ["detekt"]

--- app/src/main/java/ru/practicum/android/diploma/db/Database.kt
+++ app/src/main/java/ru/practicum/android/diploma/db/Database.kt
@@ -7,9 +7,7 @@ import ru.practicum.android.diploma.db.entity.VacancyDetailEntity
 
 @Database(
     version = 1,
-    entities = [
-        VacancyDetailEntity::class
-    ])
+    entities = [ VacancyDetailEntity::class ])
 abstract class Database : RoomDatabase() {
     abstract fun FavoritesDao(): FavoritesDao
 }
