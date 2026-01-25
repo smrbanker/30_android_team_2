4249efa6c89c0f8d6e18f2c95294039f22a49d03
Dmitry-Schukin <iliakirsanov5321@gmail.com>
2026-01-25 18:43:16 +0300

    Создание базы данных и класса-наследника Application

---
+++ app/src/main/java/ru/practicum/android/diploma/db/dao/FavoritesDao.kt
@@ -0,0 +1,25 @@
+package ru.practicum.android.diploma.db.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.db.entity.VacancyDetailEntity
+
+@Dao
+interface FavoritesDao {
+    @Insert(entity = VacancyDetailEntity::class, onConflict = OnConflictStrategy.Companion.REPLACE)
+    suspend fun insertNewFavoriteVacancy(favoriteVacancy: VacancyDetailEntity)
+
+    @Update(entity = VacancyDetailEntity::class, onConflict = OnConflictStrategy.Companion.REPLACE)
+    suspend fun updateFavoriteVacancy(favoriteVacancy: VacancyDetailEntity)
+
+    @Delete(entity = VacancyDetailEntity::class)
+    suspend fun deleteFavoriteVacancy(favoriteVacancy: VacancyDetailEntity)
+
+    @Query("SELECT * FROM favorites_vacancies_table ORDER BY insert_time DESC")
+    suspend fun getAllFavoriteVacancy(): Flow<List<VacancyDetailEntity>>
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/db/entity/VacancyDetailEntity.kt
@@ -0,0 +1,27 @@
+package ru.practicum.android.diploma.db.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "favorites_vacancies_table")
+data class VacancyDetailEntity(
+    @PrimaryKey(autoGenerate = false)
+    val id: String,                                                     //Идентификатор вакансии
+    val name: String,                                                   //Название
+    val description: String,                                            //Описание
+    val salary: String?,                                                //Зарплата
+    val address: String?,                                               //Адрес компании
+    val experience: String?,                                            //Требуемый опыт работы
+    val schedule: String?,                                              //График
+    val employment: String?,                                            //Занятость
+    val contacts: String?,                                              //Контакты для связи
+    val employer: String,                                               //Наниматель
+    val area: String,                                                   //Район
+    val skills: String,                                                 //Навыки
+    val url: String,                                                    //Ссылка на логотип
+    val industry: String,                                               //Отрасль
+    @ColumnInfo(name = "insert_time")
+    val insertTimestampMillis: Long = System.currentTimeMillis()        //Время добавления вакансии в миллисекундах
+) {
+}
