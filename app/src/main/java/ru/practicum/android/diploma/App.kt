14139289bc454e5bc3a066a5c4302ab72058e159
Zazazlik <zazazlikxxx@gmail.com>
2026-01-26 06:09:56 +0300

    Manifest, icon, light and dark themes

---
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -0,0 +1,12 @@
+package ru.practicum.android.diploma
+
+import android.app.Application
+
+class App : Application() {
+    
+    override fun onCreate() {
+        super.onCreate()
+        // todo: initialize the application
+    }
+}
+
