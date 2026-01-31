5bd4d9f5868377015b31e3ceb2e2c7d7775cfa27
Nikita Saltykov <justverlink@yandex.ru>
2026-01-31 15:10:04 +0300

    DI

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,14 +1,38 @@
 package ru.practicum.android.diploma.di
 
 import androidx.room.Room
+import com.google.gson.Gson
+import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.data.NetworkClient
+import ru.practicum.android.diploma.data.dto.models.VacancyConverter
+import ru.practicum.android.diploma.data.network.JobApiService
+import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
+import ru.practicum.android.diploma.data.repository.VacancyRepositoryImpl
 import ru.practicum.android.diploma.db.Database
+import ru.practicum.android.diploma.domain.api.VacancyInteractor
+import ru.practicum.android.diploma.domain.api.VacancyRepository
+import ru.practicum.android.diploma.domain.impl.VacancyInteractorImpl
 
 val dataModule = module {
     single {
         Room.databaseBuilder(androidContext(), Database::class.java, "database.db")
             .build()
     }
+
     single { get<Database>().FavoritesDao() }
+
+    single<JobApiService> {
+        Retrofit.Builder()
+            .baseUrl("https://practicum-diploma-8bc38133faba.herokuapp.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(JobApiService::class.java)
+    }
+
+    single<NetworkClient> {
+        RetrofitNetworkClient(get(), androidApplication())
+    }
 }
--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -1,7 +1,22 @@
 package ru.practicum.android.diploma.di
 
+import com.google.gson.Gson
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.dto.models.VacancyConverter
+import ru.practicum.android.diploma.data.repository.VacancyRepositoryImpl
+import ru.practicum.android.diploma.domain.api.VacancyRepository
 
 val repositoryModule = module {
+    factory<VacancyRepository> {
+        VacancyRepositoryImpl(get(), get())
+    }
 
+    factory {
+        Gson()
+    }
+
+
+    factory {
+        VacancyConverter(get())
+    }
 }
