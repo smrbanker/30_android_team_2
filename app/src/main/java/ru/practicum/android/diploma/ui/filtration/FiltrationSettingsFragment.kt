28a33c34c19635894883a9f45b3c9c56f2436333
smrbanker <smrbanker@gmail.com>
2026-02-06 19:55:01 +0300

    E4-2-DD work + реализация запроса с фильтрами из Sp

--- app/src/main/java/ru/practicum/android/diploma/ui/filtration/FiltrationSettingsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filtration/FiltrationSettingsFragment.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.databinding.FragmentFiltrationSettingsBinding
 import ru.practicum.android.diploma.domain.models.Filter
 import ru.practicum.android.diploma.domain.models.Location
+import ru.practicum.android.diploma.ui.filtration.FiltrationSettingsViewModel.Companion.SP_ERROR_CLEAR
+import ru.practicum.android.diploma.ui.filtration.FiltrationSettingsViewModel.Companion.SP_ERROR_INPUT
+import ru.practicum.android.diploma.ui.filtration.FiltrationSettingsViewModel.Companion.SP_ERROR_OUTPUT
 import kotlin.getValue
 
 class FiltrationSettingsFragment : Fragment() {
@@ -33,11 +37,51 @@ class FiltrationSettingsFragment : Fragment() {
 
         val viewModel by viewModel<FiltrationSettingsViewModel>()
 
+        viewModel.observeState().observe(viewLifecycleOwner) {
+            render(it)
+        }
+
         viewModel.saveFilter(f)
         viewModel.showFilter()
 
         binding.toolbar.setOnClickListener { findNavController().popBackStack() }
 
+    }
+
+    private fun render(state: FiltrationSettingsState) {
+        when (state) {
+            is FiltrationSettingsState.Content -> showContent(state.filter)
+            is FiltrationSettingsState.Error -> showError(state.message)
+        }
+    }
+
+    private fun showContent(filter: Filter) {
+        // ЗАГРУЗКА ДАННЫХ В СВОБОДНЫЕ ПОЛЯ
+    }
+
+    private fun showError(errorMessage: String) {
+        when (errorMessage) {
+            SP_ERROR_INPUT -> Toast.makeText(
+                requireContext(),
+                SP_ERROR_INPUT,
+                Toast.LENGTH_SHORT
+            )
+                .show()
+
+            SP_ERROR_OUTPUT -> Toast.makeText(
+                requireContext(),
+                SP_ERROR_OUTPUT,
+                Toast.LENGTH_SHORT
+            )
+                .show()
+
+            SP_ERROR_CLEAR -> Toast.makeText(
+                requireContext(),
+                SP_ERROR_CLEAR,
+                Toast.LENGTH_SHORT
+            )
+                .show()
+        }
     }
 
     override fun onDestroyView() {
