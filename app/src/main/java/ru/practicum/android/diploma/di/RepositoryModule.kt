7a4bc818fe3e9901ecc2e147605dcc29286eae55
smrbanker <smrbanker@gmail.com>
2026-02-06 20:17:20 +0300

    detekt

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -1,16 +1,16 @@
 package ru.practicum.android.diploma.di
 
-import org.koin.dsl.module
-import ru.practicum.android.diploma.db.FavouritesRepositoryImpl
-import ru.practicum.android.diploma.domain.db.FavouritesRepository
 import com.google.gson.Gson
+import org.koin.dsl.module
 import ru.practicum.android.diploma.data.FilterSpRepositoryImpl
 import ru.practicum.android.diploma.data.repository.SearchVacancyDetailsRepositoryImpl
-import ru.practicum.android.diploma.util.VacancyDbConverter
 import ru.practicum.android.diploma.data.repository.VacancyRepositoryImpl
+import ru.practicum.android.diploma.db.FavouritesRepositoryImpl
 import ru.practicum.android.diploma.domain.api.FilterSpRepository
 import ru.practicum.android.diploma.domain.api.SearchVacancyDetailsRepository
 import ru.practicum.android.diploma.domain.api.VacancyRepository
+import ru.practicum.android.diploma.domain.db.FavouritesRepository
+import ru.practicum.android.diploma.util.VacancyDbConverter
 
 val repositoryModule = module {
     factory<VacancyRepository> {
