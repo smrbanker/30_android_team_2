e5a43dc3ac77c73ee54cc86977ccc3f3cfcfd328
Nikita Saltykov <justverlink@yandex.ru>
2026-02-07 17:07:07 +0300

    Убрал ненужный импорт

--- app/src/main/java/ru/practicum/android/diploma/domain/api/AreasInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/AreasInteractor.kt
@@ -1,7 +1,6 @@
 package ru.practicum.android.diploma.domain.api
 
 import ru.practicum.android.diploma.domain.models.Country
-import ru.practicum.android.diploma.domain.models.Resource
 
 interface AreasInteractor {
     suspend fun getCountries(): Pair<List<Country>?, String?>
