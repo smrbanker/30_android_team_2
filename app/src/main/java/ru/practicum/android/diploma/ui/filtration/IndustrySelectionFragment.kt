2fbaf0fd44445ed003d9c91ade69a5503dfaad79
Zazazlik <zazazlikxxx@gmail.com>
2026-02-08 15:34:55 +0300

    Transition to the "Filter" screen; execution and processing of a request to retrieve a list of industries; display of the list of industries on the "Industries" screen

--- app/src/main/java/ru/practicum/android/diploma/ui/filtration/IndustrySelectionFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filtration/IndustrySelectionFragment.kt
@@ -54,7 +54,7 @@ class IndustrySelectionFragment : Fragment() {
 
     private fun configureRecyclerView() {
         _adapter = IndustryAdapter(emptyList()) { industry ->
-            // TODO: Сохранить выбранную индустрию и вернуться назад
+            // Сохранить выбранную индустрию и вернуться назад
             findNavController().popBackStack()
         }
         binding.recyclerView.adapter = adapter
